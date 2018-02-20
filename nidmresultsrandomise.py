07b4f270a17cbe54b33e281b63a839a6db9fda9c
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2018-02-20 16:55:53 +0000

    added a function to create design matrix csv

--- nidmresultsrandomise.py
+++ nidmresultsrandomise.py
@@ -20,7 +20,27 @@ def getNeuroimagingAnalysisSoftware_softwareVersion(randomisedir):
 
     return(versionNumber)
 
-gfeatdir = '/home/tommaullin/Downloads/level2.gfeat'
+def getDesignMatrix_atLocation(randomisedir):
+    output_filename = './DesignMatrix.csv'
+    #Read in design.mat file
+    design_mat_file = os.path.join(randomisedir,
+                                   'design.mat')
 
+    with open(design_mat_file, 'r') as input_file:
+        lines = input_file.readlines()
+        
+    
+    with open(output_filename, 'w') as output_file:
+        always_print = False
+        for line in lines:
+            if always_print == True:
+                output_file.writelines(line)
+            if "Matrix" in line:
+                always_print = True
+                
+    return(output_filename)
+
+gfeatdir = '/Users/maullz/Desktop/pytreat_nidmrandomise/level2.gfeat'
 print(getNeuroImagingAnalysisSoftwareType(gfeatdir))
 print(getNeuroimagingAnalysisSoftware_softwareVersion(gfeatdir))
+print(getDesignMatrix_atLocation(gfeatdir))
